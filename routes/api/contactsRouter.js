acfb2a4d00fcaa3b4ac5d050e4af91e484728c69
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-06-16 17:38:55 +0300

    create & add validateSchemas to routes

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const contactsRouter = express.Router();
 const ctrl = require("../../controllers/contacts");
-const { isValidId } = require("../../middlewares");
+const { isValidId, validateBody } = require("../../middlewares");
+const { schemas } = require("../../models/contact");
 
 contactsRouter.get("/", ctrl.getAll);
 
 contactsRouter.get("/:id", isValidId, ctrl.getById);
 
-contactsRouter.post("/", ctrl.add);
+contactsRouter.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-contactsRouter.put("/:id", isValidId, ctrl.updateById);
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateById
+);
 
-contactsRouter.patch("/:id/favorite", isValidId, ctrl.updateFavorite);
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateFavorite
+);
 
 contactsRouter.delete("/:id", isValidId, ctrl.deleteById);
 
