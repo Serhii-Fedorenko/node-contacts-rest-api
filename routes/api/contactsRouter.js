59b4efc0cb8f115fc0ea835549531eaf06e87e5b
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-06-16 17:10:57 +0300

    add id validation middleware

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const contactsRouter = express.Router();
 const ctrl = require("../../controllers/contacts");
+const { isValidId } = require("../../middlewares");
 
 contactsRouter.get("/", ctrl.getAll);
 
-contactsRouter.get("/:id", ctrl.getById);
+contactsRouter.get("/:id", isValidId, ctrl.getById);
 
 contactsRouter.post("/", ctrl.add);
 
-contactsRouter.put("/:id", ctrl.updateById);
+contactsRouter.put("/:id", isValidId, ctrl.updateById);
 
-contactsRouter.patch("/:id/favorite", ctrl.updateFavorite);
+contactsRouter.patch("/:id/favorite", isValidId, ctrl.updateFavorite);
 
-contactsRouter.delete("/:id", ctrl.deleteById);
+contactsRouter.delete("/:id", isValidId, ctrl.deleteById);
 
 module.exports = contactsRouter;
