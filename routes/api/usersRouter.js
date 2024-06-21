0697d55bbb531e0280ff0b76e7cac4740c7af794
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-06-21 19:52:15 +0300

    add register controller and router

--- routes/api/usersRouter.js
+++ routes/api/usersRouter.js
@@ -1,5 +1,10 @@
-const express = require('express');
+const express = require("express");
+const { validateBody } = require("../../middlewares");
+const { schemas } = require("../../models/user");
+const ctrl = require("../../controllers/users");
 
 const usersRouter = express.Router();
+
+usersRouter.post("/register", validateBody(schemas.authSchema), ctrl.register);
 
 module.exports = usersRouter;
