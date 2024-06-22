c961c708f4558769fd41b58c8b999e2e2a8382c7
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-06-22 18:41:24 +0300

    add pagination

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,7 +3,12 @@ const { HttpError, ctrlWrapper } = require("../utils");
 
 async function getAll(req, res) {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner });
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "email subscription");
   res.status(200).json(result);
 }
 
