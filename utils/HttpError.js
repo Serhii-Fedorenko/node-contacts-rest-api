b1249ee4f316174a2538ef680b3715f499ba2b08
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-06-22 16:21:49 +0300

    add errorMessageList to HttpError util

--- utils/HttpError.js
+++ utils/HttpError.js
@@ -1,7 +1,15 @@
-function HttpError(status, message) {
-    const error = new Error(message);
-    error.status = status;
-    return error;
+const errorMessageList = {
+  400: "Bad Request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not found",
+  409: "Conflict",
+};
+
+function HttpError(status, message = errorMessageList[status]) {
+  const error = new Error(message);
+  error.status = status;
+  return error;
 }
 
 module.exports = HttpError;
