53cbd8fb7b7c12042e1d8971d9a20a795cb2d578
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-06-22 15:53:31 +0300

    add authenticate middleware

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const contactsRouter = express.Router();
 const ctrl = require("../../controllers/contacts");
-const { isValidId, validateBody } = require("../../middlewares");
+const { isValidId, validateBody, authenticate } = require("../../middlewares");
 const { schemas } = require("../../models/contact");
 
-contactsRouter.get("/", ctrl.getAll);
+contactsRouter.get("/", authenticate, ctrl.getAll);
 
-contactsRouter.get("/:id", isValidId, ctrl.getById);
+contactsRouter.get("/:id", authenticate, isValidId, ctrl.getById);
 
-contactsRouter.post("/", validateBody(schemas.addSchema), ctrl.add);
+contactsRouter.post(
+  "/",
+  authenticate,
+  validateBody(schemas.addSchema),
+  ctrl.add
+);
 
 contactsRouter.put(
   "/:id",
+  authenticate,
   isValidId,
   validateBody(schemas.addSchema),
   ctrl.updateById
@@ -19,11 +25,12 @@ contactsRouter.put(
 
 contactsRouter.patch(
   "/:id/favorite",
+  authenticate,
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   ctrl.updateFavorite
 );
 
-contactsRouter.delete("/:id", isValidId, ctrl.deleteById);
+contactsRouter.delete("/:id", authenticate, isValidId, ctrl.deleteById);
 
 module.exports = contactsRouter;
