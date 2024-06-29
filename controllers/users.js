0bfb5160da41a1165d91ae3c72f1fb767f1d6ef1
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-06-29 18:22:13 +0300

    add upload middleware and avatar to user body

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
 const { User } = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../utils");
 
@@ -14,8 +15,13 @@ async function register(req, res) {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     email: newUser.email,
